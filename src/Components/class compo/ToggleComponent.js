2fad36d8314a7fb0cf2573a9e5c4c1b0705f74d3
shruthi093 <jayasruthi.l@gmail.com>
2022-04-21 12:59:39 +0530

    styles - adding styles

--- src/Components/class compo/ToggleComponent.js
+++ src/Components/class compo/ToggleComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ToggleComponent.css"
 class ToggleComponent extends React.Component{
     constructor(props){
         super(props)
@@ -13,17 +14,18 @@ class ToggleComponent extends React.Component{
         this.setState({isLoggedIn:!this.state.isLoggedIn})
     }
     render(){
+        const h1style={color:"red"}
         return(
             <>
-           {this.state.isLoggedIn?<div>
-               <h1>Username :{this.state.userName}</h1>
+           {this.state.isLoggedIn?<div className="logindiv">
+               <h1 style={h1style}>Username :{this.state.userName}</h1>
                <h2>Role :{this.state.role}</h2>
                <h4>CheckIn :{this.state.loggedinTime}</h4>
                <button onClick={this.changeStatus}>logout</button>
            </div>:
            <div>
            <h2>Please login in  </h2>
-           <button onClick={this.changeStatus}>login</button>
+           <button onClick={this.changeStatus} className="btn btn-primary">login</button>
            </div>}
             </>
         )
